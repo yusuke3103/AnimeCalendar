a5c0912ca05353741bd87b416eed047d175e5ced
Yusuke <yusuke3103@gmail.com>
2017-02-12 20:43:08 +0900

    Revert "marge"
    This reverts commit dcb780b03ae440c54bcddf53a46f74ff0dd7a7fd.

--- AnimeCalendar/AnimeCalendar/App.xaml.cs
+++ AnimeCalendar/AnimeCalendar/App.xaml.cs
@@ -1,30 +1,34 @@
-﻿using AnimeCalendar.Views;
-using AnimeCalendar.Views.Base;
-using Prism.Unity;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 using Xamarin.Forms;
 
 namespace AnimeCalendar
 {
-	public partial class App : PrismApplication
-	{
-		public App(IPlatformInitializer initializer = null) : base(initializer)
-		{
-		}
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-		protected override async void OnInitialized()
-		{
-			InitializeComponent();
-			await this.NavigationService.NavigateAsync("/RootPage/NavigationPage/ContractsPage");
-		}
+            MainPage = new AnimeCalendar.MainPage();
+        }
 
-		protected override void RegisterTypes()
-		{
-			this.Container.RegisterTypeForNavigation<RootPage>();
-			this.Container.RegisterTypeForNavigation<NavigationPage>();
-			this.Container.RegisterTypeForNavigation<ContractsPage>();
-			this.Container.RegisterTypeForNavigation<LeadsPage>();
-			this.Container.RegisterTypeForNavigation<AccountsPage>();
-			this.Container.RegisterTypeForNavigation<OpportunitiesPage>();
-		}
-	}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
--- AnimeCalendar/AnimeCalendar/ViewModels/MenuItem.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace AnimeCalendar.ViewModels
-{
-	public class MenuItem
-	{
-		public string Title { get; set; }
-		public string PageName { get; set; }
-	}
-}
